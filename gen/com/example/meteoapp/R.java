6abd032f86ef7e864f47e72428ca32a11910a46a
chrisESGI <barreto.christophe@gmail.com>
2013-12-13 00:50:11 +0100

    Page de recherche

--- gen/com/example/meteoapp/R.java
+++ gen/com/example/meteoapp/R.java
@@ -22,18 +22,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int search=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int linearLayoutOnglet1=0x7f080002;
-        public static final int linearLayoutOnglet2=0x7f080000;
-        public static final int textViewOnglet1=0x7f080003;
-        public static final int textViewOnglet2=0x7f080001;
+        public static final int action_settings=0x7f080007;
+        public static final int cityListView=0x7f080006;
+        public static final int cityName=0x7f080000;
+        public static final int cityPostalCode=0x7f080001;
+        public static final int linearLayoutOnglet2=0x7f080002;
+        public static final int searchButton=0x7f080004;
+        public static final int searchInput=0x7f080005;
+        public static final int textViewOnglet2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_homepage=0x7f030000;
-        public static final int fav_onglet=0x7f030001;
-        public static final int search_onglet=0x7f030002;
+        public static final int city_item=0x7f030001;
+        public static final int fav_onglet=0x7f030002;
+        public static final int search_onglet=0x7f030003;
     }
     public static final class menu {
         public static final int search_city=0x7f070000;
@@ -42,6 +47,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int fav_label=0x7f050003;
+        public static final int search_input_title=0x7f050004;
         public static final int search_label=0x7f050002;
     }
     public static final class style {
--- src/com/example/meteoapp/SearchOnglet.java
+++ src/com/example/meteoapp/SearchOnglet.java
@@ -1,13 +1,110 @@
 package com.example.meteoapp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
- 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+
+
 public class SearchOnglet extends Activity {
     /** Called when the activity is first created. */
+	private ListView cityListView;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_onglet);
-    }
+        ArrayList<HashMap<String, String>> cityList = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> cityItem;
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Athis-Mons");
+        cityItem.put("cityPostalCode", "91200");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Ballancourt");
+        cityItem.put("cityPostalCode", "91610");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Corbeil-Essonne");
+        cityItem.put("cityPostalCode", "91200");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Evry");
+        cityItem.put("cityPostalCode", "91000");
+        cityList.add(cityItem);
+        
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Athis-Mons");
+        cityItem.put("cityPostalCode", "91200");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Ballancourt");
+        cityItem.put("cityPostalCode", "91610");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Corbeil-Essonne");
+        cityItem.put("cityPostalCode", "91200");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Evry");
+        cityItem.put("cityPostalCode", "91000");
+        cityList.add(cityItem);
+
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Athis-Mons");
+        cityItem.put("cityPostalCode", "91200");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Ballancourt");
+        cityItem.put("cityPostalCode", "91610");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Corbeil-Essonne");
+        cityItem.put("cityPostalCode", "91200");
+        cityList.add(cityItem);
+ 
+        cityItem = new HashMap<String, String>();
+        cityItem.put("cityName", "Evry");
+        cityItem.put("cityPostalCode", "91000");
+        cityList.add(cityItem);
+
+        cityListView = (ListView) findViewById(R.id.cityListView); 
+        SimpleAdapter adaptater = new SimpleAdapter (this.getBaseContext(), cityList, R.layout.city_item, new String[] {"cityName", "cityPostalCode"}, new int[] {R.id.cityName, R.id.cityPostalCode});
+        cityListView.setAdapter(adaptater);
+        CityList.getListViewSize(cityListView);
+        
+      //Enfin on met un écouteur d'évènement sur notre listView
+        cityListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+        	@SuppressWarnings("unchecked")
+         	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+				//on récupère la HashMap contenant les infos de notre item (titre, description, img)
+        		HashMap<String, String> map = (HashMap<String, String>) cityListView.getItemAtPosition(position);
+        		//on créer une boite de dialogue
+        		AlertDialog.Builder adb = new AlertDialog.Builder(SearchOnglet.this);
+        		//on attribut un titre à notre boite de dialogue
+        		adb.setTitle("Sélection Item");
+        		//on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
+        		adb.setMessage("Votre choix : "+map.get("cityName"));
+        		//on indique que l'on veut le bouton ok à notre boite de dialogue
+        		adb.setPositiveButton("Ok", null);
+        		//on affiche la boite de dialogue
+        		adb.show();
+        	}
+         });
+     }
 }
